e95731fbe0735c2d9d2659161f60a01d11a722fb
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-29 19:57:39 +0000

    Tenho a impressão que não está sendo reduzido o tamanho da fonte dos tit

--- src/components/landing/ResultsSection.tsx
+++ src/components/landing/ResultsSection.tsx
@@ -6,7 +6,7 @@ const ResultsSection = () => {
     <section id="results" className="w-full bg-secondary py-12">
       <div className="container mx-auto px-4 md:px-6 text-center space-y-12">
         <div className="space-y-4 animate-on-scroll">
-          <h2 className="text-4xl md:text-4xl font-headline font-bold">
+          <h2 className="text-3xl md:text-4xl font-headline font-bold">
             Resultados reais de mulheres que confiaram no Método S.L.I.M
           </h2>
         </div>
